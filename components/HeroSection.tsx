1219efe3888d0ff6f6ca66c746699b962c28d1ce
Hassan <hassaniftikhar92954@gmail.com>
2025-11-10 23:23:22 +0500

    video

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -98,7 +98,7 @@ export default function HeroSection() {
             controls={false}
             className="w-full h-full object-cover"
           >
-            <source src="/hero-video2.mp4" type="video/mp4" />
+            <source src="/hero-video.mp4" type="video/mp4" />
           </video>
           <div className="absolute inset-0 bg-black/50" />
         </div>,
